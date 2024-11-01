befb8963ab16a8fe560811e7d7f3b5d1ff4f00a8
Duke Kongo <dukekongo16@gmail.com>
2024-11-01 12:29:58 +0000

    First Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+TF_ENABLE_ONEDNN_OPTS=0
+
+from flask import Flask, render_template, Response
+from datatesting import generate_frames
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
